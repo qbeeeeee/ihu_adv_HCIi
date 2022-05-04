54bb85e66c9eb99b1cec32ffb6f0a02be525084a
Konstantinos papadopoylos <foxfireninetails9@gmail.com>
2022-05-04 21:17:44 +0300

    adv_hci app homepage

--- app/src/main/java/com/example/testingqbee/emptyActivity.java
+++ app/src/main/java/com/example/testingqbee/emptyActivity.java
@@ -1,13 +1,24 @@
 package com.example.testingqbee;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
-public class emptyActivity extends Fragment {
+public class emptyActivity extends Fragment implements View.OnClickListener {
+    CardView cSettings;
+    CardView cInsert;
+    CardView cDelete;
+    CardView cUpdate;
     public emptyActivity(){
 
     }
@@ -16,6 +27,37 @@ public class emptyActivity extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.welcome, container, false);
+        cSettings = view.findViewById(R.id.cardSettings);
+        cSettings.setOnClickListener(this);
+        cInsert = view.findViewById(R.id.cardInsert);
+        cInsert.setOnClickListener(this);
+        cUpdate = view.findViewById(R.id.cardUpdate);
+        cUpdate.setOnClickListener(this);
+        cDelete = view.findViewById(R.id.cardDelete);
+        cDelete.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.cardSettings:
+                openActivity2();
+                break;
+            case R.id.cardInsert:
+                MainActivity.fragmentManager.beginTransaction().replace(R.id.fragment_container, new insertFragment()).addToBackStack(null).commit();
+                break;
+            case R.id.cardDelete:
+                MainActivity.fragmentManager.beginTransaction().replace(R.id.fragment_container, new deleteFragment()).addToBackStack(null).commit();
+                break;
+            case R.id.cardUpdate:
+                MainActivity.fragmentManager.beginTransaction().replace(R.id.fragment_container, new updateFragment()).addToBackStack(null).commit();
+                break;
+        }
+    }
+    public void openActivity2(){
+        Intent intent = new Intent(getActivity(),settingsActivity.class);
+        startActivity(intent);
     }
 }
